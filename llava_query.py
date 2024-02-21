340aeb75fbb12978bdd94148424701ff07da7263
hegde95 <hegdeshashank@yahoo.com>
2024-02-20 17:09:05 -0800

    llava querier class done

--- llava_query.py
+++ llava_query.py
@@ -7,7 +7,7 @@ from io import BytesIO
 from PIL import Image
 import time
 
-from llava.conversation import default_conversation, conv_templates
+from conversation import default_conversation, conv_templates
 import hashlib
 
 
@@ -101,6 +101,68 @@ def main():
     print("")
 
 
+class LLAVAQuerier():
+    def __init__(self, model_name, worker_address, controller_address, message):
+        self.model_name = model_name
+        self.worker_address = worker_address
+        self.controller_address = controller_address
+        self.message = message
+
+        if self.worker_address:
+            self.worker_addr = self.worker_address
+        else:
+            controller_addr = self.controller_address
+            ret = requests.post(controller_addr + "/refresh_all_workers")
+            ret = requests.post(controller_addr + "/list_models")
+            models = ret.json()["models"]
+            models.sort()
+            print(f"Models: {models}")
+
+            ret = requests.post(controller_addr + "/get_worker_address",
+                json={"model": args.model_name})
+            worker_addr = ret.json()["address"]
+            print(f"worker_addr: {worker_addr}")    
+            self.worker_addr = worker_addr    
+        
+        if self.worker_addr == "":
+            raise Exception("Worker address is empty")
+
+        # self.conv = conv_templates["llava_v1"].copy()
+    
+    def query(self, image):
+        conv = conv_templates["llava_v1"].copy()
+        conv.append_message(conv.roles[0], (self.message, image, "Default"))
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+
+        all_image_hash = conv.get_images()
+
+        headers = {"User-Agent": "LLaVA Client"}
+
+        pload = {
+            'model': 'llava-v1.5-7b', 
+            'prompt': prompt, 
+            'temperature': 0.2, 
+            'top_p': 0.7, 
+            'max_new_tokens': 512, 
+            'stop': '</s>', 
+            'images': f"List of 1 images: {all_image_hash}"
+        }
+        pload['images'] = conv.get_images()        
+
+        response = requests.post(self.worker_addr + "/worker_generate_stream",
+            headers=headers, json=pload, stream=True, timeout=10)
+        for chunk in response.iter_lines(decode_unicode=False, delimiter=b"\0"):
+            if chunk:
+                data = json.loads(chunk.decode())
+                if data["error_code"] == 0:
+                    output = data["text"][len(prompt):].strip()
+                else:
+                    output = data["text"] + f" (error_code: {data['error_code']})"   
+        return output     
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--controller-address", type=str, default="http://localhost:10000")
@@ -108,8 +170,12 @@ if __name__ == "__main__":
     parser.add_argument("--model-name", type=str, default="llava-v1.5-7b")
     parser.add_argument("--max-new-tokens", type=int, default=32)
     parser.add_argument("--message", type=str, default=
-        "<image> You are controlling a robot. The robot has to navigate to 5 cm in front of the tick mark. You available actions are walk straight, a: walk left, b: walk right, c: turn back, d: Do nothing. Choose one."
-        # "<image> What is unusual about this image?"         
+        # "<image> You are controlling a robot. The robot has to navigate to 5 cm in front of the tick mark. You available actions are walk straight, a: walk left, b: walk right, c: turn back, d: Do nothing. Choose one."
+        # "<image> What is unusual about this image?"    
+        "<image> Do you see a tick mark? reply with a simple yes or no."     
         )
     args = parser.parse_args()
-    main()
+    # main()
+    image = Image.open("tmp.png")
+    querier = LLAVAQuerier(args.model_name, args.worker_address, args.controller_address, args.message)
+    print(querier.query(image))
---
+++ serialMaster/quadController.py
@@ -0,0 +1,41 @@
+from ardSerial import *
+
+
+class QuadController:
+    def __init__(self,):
+        connectPort(goodPorts)
+        t = threading.Thread(target=keepCheckingPort, args=(goodPorts,))
+        t.start()
+        if len(sys.argv) >= 2:
+            if len(sys.argv) == 2:
+                cmd = sys.argv[1]
+                token = cmd[0][0]
+            else:
+                token = sys.argv[1][0]
+            #                sendTaskParallel([sys.argv[1][0], sys.argv[1:], 1])
+            send(goodPorts, [sys.argv[1][0], sys.argv[1:], 1])
+        printH('Model list',config.modelList)
+        print("You can type 'quit' or 'q' to exit.")
+    
+    def sendTask(self, x, ports, time_taken=1, following_behavior="up"):
+        if x != "":
+            if x == "q" or x == "quit":
+                return False
+            else:
+                token = x[0]
+                task = x[1:].split()  # white space
+                # if len(task) <= 1:
+                send(ports, [x, 1])
+                time.sleep(time_taken)
+                send(ports, ["kup", 1])
+                # else:
+                #     send(ports, [token, list(map(int, task)), 1])
+        
+
+if __name__ == "__main__":
+    quadController = QuadController()
+    while True:
+        x = input("Enter command: ")
+        if not quadController.sendTask(x, goodPorts):
+            break
+    print("Exiting...")
